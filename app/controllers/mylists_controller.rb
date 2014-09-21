3b694d3042be5d102882701e17251ba14c8db08b
Masami Yonehara <zeitdiebe@gmail.com>
2014-09-21 19:15:30 +0900

    remove unneeded response logics

--- app/controllers/mylists_controller.rb
+++ app/controllers/mylists_controller.rb
@@ -28,10 +28,8 @@ class MylistsController < ApplicationController
 
     respond_to do |format|
       if @mylist.save
-        format.html { redirect_to @mylist, notice: 'Mylist was successfully created.' }
         format.json { render :show, status: :created, location: @mylist }
       else
-        format.html { render :new }
         format.json { render json: @mylist.errors, status: :unprocessable_entity }
       end
     end
@@ -42,10 +40,8 @@ class MylistsController < ApplicationController
   def update
     respond_to do |format|
       if @mylist.update(mylist_params)
-        format.html { redirect_to @mylist, notice: 'Mylist was successfully updated.' }
         format.json { render :show, status: :ok, location: @mylist }
       else
-        format.html { render :edit }
         format.json { render json: @mylist.errors, status: :unprocessable_entity }
       end
     end
@@ -56,7 +52,6 @@ class MylistsController < ApplicationController
   def destroy
     @mylist.destroy
     respond_to do |format|
-      format.html { redirect_to mylists_url, notice: 'Mylist was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
